6a717eb778ac947cacf5d21eafbac092616d1434
Sam~Lubbers <lubbers.sc@gmail.com>
2019-12-23 10:02:32 +0100

    datetime recording

--- backup_script.py
+++ backup_script.py
@@ -2,14 +2,14 @@
 import os
 from dotenv import load_dotenv
 import json
-from datetime import date
+from datetime import datetime
 from rebel_management_utilities import get_all_members
 from os import path
 
 
 def make_backup(api_key):
     data = {'members': get_all_members(api_key)}
-    current_date_str = date.now().strftime("%d-%m-%Y_%H:%M:%S")
+    current_date_str = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
 
     file_path = path.join('backups', f'backup_rebels_{current_date_str}.json')
 
@@ -25,6 +25,7 @@ def load_api_key():
         raise OSError('ACTION_NETWORK_API_KEY not found in .env')
 
     return key
+
 
 if __name__ == '__main__':
     api_key = load_api_key()
